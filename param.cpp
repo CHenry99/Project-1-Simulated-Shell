d72610285fb9624b3509300bed8a59d47900fe2d
newhallt <59701168+newhallt@users.noreply.github.com>
2021-09-11 23:18:54 -0500

    Update param.cpp

--- param.cpp
+++ param.cpp
@@ -37,6 +37,33 @@ void Param::PrintParams(){
 	    cout << "ArugmentVector[" << i << "]: [" <<
 		    argumentVector[i] << "]" << endl;
 	}
+	
+	
+	
+	
+	/*if we used what the example he gvae, the program won't run for some  reason, so this is what I came up with - Thitari :
+	cout << "InputRedirect: [";
+	if(inputRedirect != NULL){
+		cout << inputRedirect << "]" << endl;
+	}
+	else {
+		cout << "NULL" << endl;
+	}
+	cout << "OutputRedirect: [";
+	if(outputRedirect != NULL){
+		cout << outputRedirect << "]" << endl;
+	}
+	else {
+		cout << "NULL" << endl;
+	}
+	for(int i = 0; i < argumentCount; i++)
+	{
+		cout << "ArgumentVector[" << i << "]: [" << argumentVector[i] << "]" << endl;
+	}
+}
+*/
+	
+	
 }
 
 /**
